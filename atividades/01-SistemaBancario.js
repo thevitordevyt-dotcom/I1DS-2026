f7cd4da4fc5d7cba5c1b4bd980ac6a0d16130ab1
thevitordevyt-dotcom <thevitordevyt-dotcom>
2026-03-11 14:31:58 -0300

    Criando um sistema bancario simples

---
+++ atividades/01-SistemaBancario.js
@@ -0,0 +1,56 @@
+/***************************************************** 
+Exercícios para fixação 
+*****************************************************/ 
+/* 
+1. Crie uma variável e atribua o nome de uma pessoa; 
+2. Crie uma variável e atribua o nome de um banco; 
+3. Crie uma variável e atribua o número da agência; 
+4. Crie uma variável e atribua o número da conta; 
+5. Crie uma variável e atribua o saldo da conta; 
+6. Imprima os dados e o saldo da conta; 
+7. Efetue e imprima 5 movimentações na conta; 
+8. Imprima novamente os dados e o saldo da conta; 
+*/
+
+//Criação de sistema bancario
+
+let nome = "poyo"
+let banco = "Nupank"
+let agencia = "001"
+let conta = 170
+let saldo = 9900
+let CDB = 20
+let tesouro = 500
+let bolsa = 10000
+let imobiliario = 500
+let fiis = 700
+
+console.log("----------------------------------------")
+console.log("digitalizando alguns dados e imprimindo saldo:");
+console.log("Valores: saldo ", saldo);
+console.log("Nome do operador: nome", nome);
+console.log("Nome da agência: banco", banco);
+console.log("Número da conta: conta", conta);
+
+console.log("----------------------------------------");
+console.log("Recebendo dividendos diários de investimentos!");
+console.log("recebendo liquidez diária de CDB!");
+console.log("Pix recebido: CDB", CDB)
+console.log("Recebendo saldo do tesouro direto!")
+console.log("Pix recebido: tesouro", tesouro)
+console.log("Recebendo retorno da bolsa de valores")
+console.log("Pix recebido: bolsa", bolsa)
+console.log("Recebendo dividendos do mercado imobiliario")
+console.log("Pix recebido: imobiliario", imobiliario)
+console.log("Recebendo dividendos de fiis")
+console.log("Pix recebido: fiis", fiis)
+
+console.log("CDB + tesouro + bolsa + imobiliario + fiis =",CDB + tesouro + bolsa + imobiliario + fiis)
+
+console.log("----------------------------------------")
+console.log("digitalizando alguns dados e imprimindo saldo:");
+console.log("Valores: CDB + tesouro + bolsa + imobiliario + fiis ", CDB + tesouro + bolsa + imobiliario + fiis);
+console.log("Nome do operador: nome", nome);
+console.log("Nome da agência: banco", banco);
+console.log("Número da conta: conta", conta);
+
