4de78d940133339908b8c5f1eedca059b48f0f50
thevitordevyt-dotcom <thevitordevyt-dotcom>
2026-03-24 10:09:17 -0300

    Incremento para realização do laço de repetição

---
+++ aulas/07 - incremento.js
@@ -0,0 +1,17 @@
+// Incremento parte 2 - revisão
+// Incremento = Somar algo a uma variavel
+// descremento = subtrair algo a uma variavel
+let idade = 16;
+console.log(idade)
+
+//idade = idade + 1 ; // Somei 1 na idade
+idade++;
+console.log(idade);
+idade--
+console.log(idade);
+
+let novaIdade = idade++; // Idade++ Soma na mesma variavel
+console.log("idade",idade,"| Nova idade :", novaIdade)
+let novaidade = ++idade
+console.log("idade",idade,"| Nova idade :", novaIdade)
+
---
+++ aulas/12 - lacoRepeticao.js
@@ -0,0 +1,4 @@
+// O professor te colocou de castigo e pediu para escrever mil vezes a frase!
+// Eu vou prestar atenção nas aulas e anotar tudo!
+// laço de repetição for (para)
+    
